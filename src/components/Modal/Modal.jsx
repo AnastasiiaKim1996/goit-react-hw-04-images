2b6e99a147bc4dac32b56f6eeaaa24ed493eb12b
Анастасия Маслак <96750008+AnastasiiaKim1996@users.noreply.github.com>
2022-10-25 20:47:04 +0300

    refactoring/ add hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Backdrop, PopUp } from 'components/Modal/Modal.styled';
 
 const modal = document.getElementById('backdrop');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+export const Modal = ({ children, onClose }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+  });
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+  useEffect(() => {
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
 
   // закриває модалку по ескейп
 
-  handleKeyDown = event => {
+  const handleKeyDown = event => {
     const { code } = event;
-    const { onClose } = this.props;
     if (code === 'Escape') {
       onClose();
     }
@@ -25,20 +26,55 @@ export class Modal extends Component {
 
   // закриває модалку по бекдропу
 
-  handleBackdropClick = e => {
-    const { onClose } = this.props;
+  const handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
       onClose();
     }
   };
 
-  render() {
-    const { children } = this.props;
-    return createPortal(
-      <Backdrop onClick={this.handleBackdropClick}>
-        <PopUp>{children}</PopUp>
-      </Backdrop>,
-      modal
-    );
-  }
-}
+  return createPortal(
+    <Backdrop onClick={handleBackdropClick}>
+      <PopUp>{children}</PopUp>
+    </Backdrop>,
+    modal
+  );
+};
+
+// export class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   // закриває модалку по ескейп
+
+//   handleKeyDown = event => {
+//     const { code } = event;
+//     const { onClose } = this.props;
+//     if (code === 'Escape') {
+//       onClose();
+//     }
+//   };
+
+//   // закриває модалку по бекдропу
+
+//   handleBackdropClick = e => {
+//     const { onClose } = this.props;
+//     if (e.currentTarget === e.target) {
+//       onClose();
+//     }
+//   };
+
+//   render() {
+//     const { children } = this.props;
+//     return createPortal(
+//       <Backdrop onClick={this.handleBackdropClick}>
+//         <PopUp>{children}</PopUp>
+//       </Backdrop>,
+//       modal
+//     );
+//   }
+// }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import { ImSearch } from 'react-icons/im';
@@ -14,62 +14,115 @@ import {
   Green,
 } from 'components/Searchbar/Searchbar.styled.jsx';
 
-export class Searchbar extends Component {
-  state = {
-    imgName: '',
-  };
+export const Searchbar = ({ searchValue }) => {
+  const [imgName, setImgName] = useState('');
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
     // при сабміті форми викл метод з Арр і передаємо йому значення стейту imgName//
 
-    this.props.searchValue(this.state.imgName);
-    this.setState({ imgName: '' });
+    searchValue(imgName);
+    setImgName('');
   };
 
-  handleChangeImg = e => {
-    this.setState({ imgName: e.currentTarget.value.toLowerCase() });
+  const handleChangeImg = e => {
+    setImgName(e.currentTarget.value.toLowerCase());
   };
 
-  render() {
-    return (
-      <header>
-        <Form onSubmit={this.handleSubmit}>
-          <a
-            href="https://anastasiiakim1996.github.io/goit-react-hw-03-image-finder/"
-            title=""
-          >
-            <Title>
-              Image <Blue>S</Blue>
-              <Red>e</Red>
-              <Yellow>a</Yellow>
-              <Blue>r</Blue>
-              <Green>c</Green>
-              <Red>h</Red>
-            </Title>
-          </a>
+  return (
+    <header>
+      <Form onSubmit={handleSubmit}>
+        <a
+          href="https://anastasiiakim1996.github.io/goit-react-hw-03-image-finder/"
+          title=""
+        >
+          <Title>
+            Image <Blue>S</Blue>
+            <Red>e</Red>
+            <Yellow>a</Yellow>
+            <Blue>r</Blue>
+            <Green>c</Green>
+            <Red>h</Red>
+          </Title>
+        </a>
 
-          <Wrapper>
-            <Input
-              type="text"
-              id={nanoid()}
-              placeholder="Search images and photos"
-              value={this.state.imgName} // записуємо значення до стейту //
-              onChange={this.handleChangeImg}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-            <Btn type="submit" className={CSS.button}>
-              <ImSearch />
-            </Btn>
-          </Wrapper>
-        </Form>
-      </header>
-    );
-  }
-}
+        <Wrapper>
+          <Input
+            type="text"
+            id={nanoid()}
+            placeholder="Search images and photos"
+            value={imgName} // записуємо значення до стейту //
+            onChange={handleChangeImg}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <Btn type="submit" className={CSS.button}>
+            <ImSearch />
+          </Btn>
+        </Wrapper>
+      </Form>
+    </header>
+  );
+};
+
+// export class Searchbar extends Component {
+//   state = {
+//     imgName: '',
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     // при сабміті форми викл метод з Арр і передаємо йому значення стейту imgName//
+
+//     this.props.searchValue(this.state.imgName);
+//     this.setState({ imgName: '' });
+//   };
+
+//   handleChangeImg = e => {
+//     this.setState({ imgName: e.currentTarget.value.toLowerCase() });
+//   };
+
+//   render() {
+//     return (
+//       <header>
+//         <Form onSubmit={this.handleSubmit}>
+//           <a
+//             href="https://anastasiiakim1996.github.io/goit-react-hw-03-image-finder/"
+//             title=""
+//           >
+//             <Title>
+//               Image <Blue>S</Blue>
+//               <Red>e</Red>
+//               <Yellow>a</Yellow>
+//               <Blue>r</Blue>
+//               <Green>c</Green>
+//               <Red>h</Red>
+//             </Title>
+//           </a>
+
+//           <Wrapper>
+//             <Input
+//               type="text"
+//               id={nanoid()}
+//               placeholder="Search images and photos"
+//               value={this.state.imgName} // записуємо значення до стейту //
+//               onChange={this.handleChangeImg}
+//               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//               required
+//             />
+//             <Btn type="submit" className={CSS.button}>
+//               <ImSearch />
+//             </Btn>
+//           </Wrapper>
+//         </Form>
+//       </header>
+//     );
+//   }
+// }
 
 Searchbar.propTypes = {
   searchValue: PropTypes.func.isRequired,
