7101e19ef28e2cf7e83557d4a5b68bdcf45deaa8
Анастасия Маслак <96750008+AnastasiiaKim1996@users.noreply.github.com>
2022-10-26 18:26:24 +0300

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { useState, useEffect } from 'react';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Searchbar } from './Searchbar/Searchbar';
 import { Loader } from 'components/Loader/Loader';
@@ -8,146 +8,104 @@ import { FetchImg } from './Api/FetchImg';
 import { LoadMoreBtn } from './Button/LoadMoreBtn';
 import { ErrorMessage } from './ErrorMessage/ErrorMessage';
 import { Modal } from './Modal/Modal';
+import { toastSuccess, toastError, toastWarn } from './Notification/Toasts';
+
 import brokenImg from 'components/Img/brokenImage.png';
 
-export class App extends Component {
-  state = {
-    images: [],
-    searchValue: '',
-    page: 1,
-    loading: false,
-    error: null,
-    modalIsOpen: false,
-    modalImage: '',
-  };
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalImage, setModalImage] = useState('');
 
-  // методи життєвого циклу
+  const isImg = Boolean(images.length);
 
-  async componentDidUpdate(prevProps, prevState) {
-    try {
-      const { images, page, searchValue, modalImage } = this.state;
-
-      if (prevState.searchValue !== searchValue || prevState.page !== page) {
-        this.setState({ loading: true });
-        await FetchImg(searchValue, page).then(data => {
-          return data.hits.length === 0
-            ? this.setState({ error: true })
-            : this.setState(img => ({
-                images: [...img.images, ...data.hits],
-                error: false,
-              }));
-        });
-        this.setState({ loading: false });
-      }
-      if (images.length > 1 && page === 1 && modalImage === '') {
-        this.toastSuccess();
-      }
-    } catch (error) {
-      return this.toastError();
+  useEffect(() => {
+    if (searchValue === '') {
+      return;
     }
-  }
+    setLoading(true);
+    FetchImg(searchValue, page)
+      .then(({ totalHits, hits }) => {
+        if (totalHits === 0) {
+          toastError();
+          setLoading(false);
+          setError(true);
+          return;
+        }
+        if (page === 1 && hits.length > 1) {
+          toastSuccess();
+        }
+        const data = hits.map(({ id, tags, webformatURL, largeImageURL }) => ({
+          id,
+          tags,
+          webformatURL,
+          largeImageURL,
+        }));
+        setImages(img => [...img, ...data]);
+        setLoading(false);
+        setError(false);
+      })
+      .catch(error => toastError())
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [searchValue, page]);
 
   // метод додавання нових картинок через btn load more
 
-  LoadMore = () => {
-    this.setState(({ page }) => {
-      return {
-        page: page + 1,
-      };
-    });
+  const LoadMore = () => {
+    setPage(page + 1);
   };
 
   //перекидання значення з інпуту через пропс з (searchbar) у основний стейт
 
-  handleFormSubmit = searchValue => {
-    if (this.state.searchValue === searchValue) {
-      this.toastWarn();
+  const handleFormSubmit = searchText => {
+    if (searchValue === searchText.trim()) {
+      toastWarn();
     }
-    this.setState({ searchValue, page: 1, images: [] });
+    setSearchValue(searchText);
+    setPage(1);
+    setImages([]);
   };
 
   //відкриття && закриття модалки
 
-  onToggleModal = () => {
-    this.setState(({ modalIsOpen }) => ({
-      modalIsOpen: !modalIsOpen,
-    }));
-    this.toastInfoCloseModal();
+  const onToggleModal = () => {
+    setModalIsOpen(!modalIsOpen);
   };
 
-  openModal = largeImageURL => {
-    this.setState({
-      modalIsOpen: true,
-      modalImage: largeImageURL,
-    });
-    this.toastInfoOpenModal();
+  const openModal = largeImageURL => {
+    setModalIsOpen(true);
+    setModalImage(largeImageURL);
   };
 
-  // нотіфікашки
+  return (
+    <div>
+      <Searchbar searchValue={handleFormSubmit} />
+      {loading && <Loader />}
+      {error === true ? (
+        <ErrorMessage errorImg={brokenImg} />
+      ) : (
+        images.length > 0 && (
+          <ImageGallery images={images} onImgClick={openModal} />
+        )
+      )}
+      {isImg && (
+        <LoadMoreBtn type="button" loadMoreImg={LoadMore}>
+          Load more
+        </LoadMoreBtn>
+      )}
+      {modalIsOpen && (
+        <Modal onClose={onToggleModal}>
+          <img src={modalImage} alt="largeImage" />
+        </Modal>
+      )}
 
-  toastSettings = {
-    theme: 'colored',
-  };
-
-  toastSuccess = () => {
-    return toast.success(
-      'Hooray! We found what you were looking for 😉',
-      this.toastSettings
-    );
-  };
-  toastError = () => {
-    return toast.error(
-      'Oops, something went wrong, please try again 🙊',
-      this.toastSettings
-    );
-  };
-  toastWarn = () => {
-    return toast.warn(
-      'Sorry, you already found this picture, please check if this will be a new search 😇',
-      this.toastSettings
-    );
-  };
-  toastInfoOpenModal = () => {
-    return toast.info(
-      'Now you can view the full-size image 😊',
-      this.toastSettings
-    );
-  };
-  toastInfoCloseModal = () => {
-    return toast.info(
-      'Okey, you can view the more images 🤭 ',
-      this.toastSettings
-    );
-  };
-
-  render() {
-    const { loading, images, error, modalIsOpen, modalImage } = this.state;
-    const isImg = Boolean(images.length);
-
-    return (
-      <div>
-        <Searchbar searchValue={this.handleFormSubmit} />
-        {loading && <Loader />}
-        {error === true ? (
-          <ErrorMessage errorImg={brokenImg} />
-        ) : (
-          images.length > 0 && (
-            <ImageGallery images={images} onImgClick={this.openModal} />
-          )
-        )}
-        {isImg && (
-          <LoadMoreBtn type="button" loadMoreImg={this.LoadMore}>
-            Load more
-          </LoadMoreBtn>
-        )}
-        {modalIsOpen && (
-          <Modal onClose={this.onToggleModal}>
-            <img src={modalImage} alt="largeImage" />
-          </Modal>
-        )}
-
-        <ToastContainer autoClose={2000} />
-      </div>
-    );
-  }
-}
+      <ToastContainer autoClose={2000} />
+    </div>
+  );
+};
